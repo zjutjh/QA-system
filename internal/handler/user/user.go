4c0da170c7ce5d6a424c015ff65df247d3587221
Penryn <15158052130@163.com>
2024-12-05 11:52:25 +0800

    feat: 新增用户获取投票题已选选项数目的接口

--- internal/handler/user/user.go
+++ internal/handler/user/user.go
@@ -3,12 +3,16 @@ package user
 import (
 	"QA-System/internal/dao"
 	"QA-System/internal/handler/queue"
+	"QA-System/internal/models"
 	"QA-System/internal/pkg/code"
 	"QA-System/internal/pkg/queue/asynq"
 	"QA-System/internal/pkg/utils"
 	"QA-System/internal/service"
 	"errors"
+	"gorm.io/gorm"
+	"sort"
 	"strconv"
+	"strings"
 
 	"time"
 
@@ -274,4 +278,148 @@ func Oauth(c *gin.Context) {
 		return
 	}
 	utils.JsonSuccessResponse(c, nil)
+}
+
+type GetOptionCount struct {
+	SerialNum int    `json:"serial_num"` //选项序号
+	Content   string `json:"content"`    //选项内容
+	Count     int    `json:"count"`      //选项数量
+}
+
+type GetSurveyStatisticsResponse struct {
+	SerialNum    int              `json:"serial_num"`    //问题序号
+	Question     string           `json:"question"`      //问题内容
+	QuestionType int              `json:"question_type"` //问题类型  1:单选 2:多选
+	Options      []GetOptionCount `json:"options"`       //选项内容
+}
+
+func GetSurveyStatistics(c *gin.Context) {
+	var data GetSurveyData
+	err := c.ShouldBindQuery(&data)
+	if err != nil {
+		c.Error(&gin.Error{Err: errors.New("获取参数失败原因: " + err.Error()), Type: gin.ErrorTypeAny})
+		utils.JsonErrorResponse(c, code.ParamError)
+		return
+	}
+	survey, err := service.GetSurveyByID(data.ID)
+	if err != nil {
+		c.Error(&gin.Error{Err: errors.New("获取问卷失败原因: " + err.Error()), Type: gin.ErrorTypeAny})
+		utils.JsonErrorResponse(c, code.ServerError)
+		return
+	}
+	if survey.Status != 2 {
+		c.Error(&gin.Error{Err: errors.New("问卷未开放"), Type: gin.ErrorTypeAny})
+		utils.JsonErrorResponse(c, code.SurveyNotOpen)
+		return
+	}
+	if survey.Type != 2 {
+		c.Error(&gin.Error{Err: errors.New("问卷为调研问卷"), Type: gin.ErrorTypeAny})
+		utils.JsonErrorResponse(c, code.ServerError)
+		return
+	}
+	answersheets, err := service.GetSurveyAnswersBySurveyID(data.ID)
+	if err != nil {
+		c.Error(&gin.Error{Err: errors.New("获取问卷收集数据失败原因: " + err.Error()), Type: gin.ErrorTypeAny})
+		utils.JsonErrorResponse(c, code.ServerError)
+		return
+	}
+
+	questionIDs := make([]int, 0)
+	for _, sheet := range answersheets {
+		for _, answer := range sheet.Answers {
+			if answer.QuestionID != 0 {
+				questionIDs = append(questionIDs, answer.QuestionID)
+			}
+		}
+	}
+
+	questions, err := service.GetQuestionsByIDs(questionIDs)
+	if err != nil {
+		c.Error(&gin.Error{Err: errors.New("获取问题信息失败原因: " + err.Error()), Type: gin.ErrorTypeAny})
+		utils.JsonErrorResponse(c, code.ServerError)
+		return
+	}
+
+	questionMap := make(map[int]models.Question)
+	for _, question := range questions {
+		questionMap[question.ID] = question
+	}
+
+	optionCounts := make(map[int]map[int]int)
+	for _, sheet := range answersheets {
+		for _, answer := range sheet.Answers {
+			question := questionMap[answer.QuestionID]
+			if question.QuestionType == 1 || question.QuestionType == 2 {
+				answerOptions := strings.Split(answer.Content, "┋")
+				for _, answerOption := range answerOptions {
+					option, err := service.GetOptionByQIDAndAnswer(answer.QuestionID, answerOption)
+					if err == gorm.ErrRecordNotFound {
+						// 则说明是其他选项，计为其他
+						if optionCounts[question.ID] == nil {
+							optionCounts[question.ID] = make(map[int]int)
+						}
+						optionCounts[question.ID][0]++
+						continue
+
+					} else if err != nil {
+						c.Error(&gin.Error{Err: errors.New("获取选项信息失败原因: " + err.Error()), Type: gin.ErrorTypeAny})
+						utils.JsonErrorResponse(c, code.ServerError)
+						return
+					}
+					if optionCounts[question.ID] == nil {
+						optionCounts[question.ID] = make(map[int]int)
+					}
+					optionCounts[question.ID][option.SerialNum]++
+				}
+			}
+		}
+	}
+	response := make([]GetSurveyStatisticsResponse, 0, len(optionCounts))
+	for qid, options := range optionCounts {
+		q := questionMap[qid]
+		var qOptions []GetOptionCount
+		if q.OtherOption {
+			qOptions = make([]GetOptionCount, 0, len(options)+1)
+			// 添加其他选项
+			qOptions = append(qOptions, GetOptionCount{
+				SerialNum: 0,
+				Content:   "其他",
+				Count:     options[0],
+			})
+		} else {
+			qOptions = make([]GetOptionCount, 0, len(options))
+		}
+
+		// 按序号排序
+		sortedSerialNums := make([]int, 0, len(options))
+		for oSerialNum := range options {
+			sortedSerialNums = append(sortedSerialNums, oSerialNum)
+		}
+		sort.Ints(sortedSerialNums)
+		for _, oSerialNum := range sortedSerialNums {
+			if oSerialNum == 0 {
+				continue
+			}
+			count := options[oSerialNum]
+			op, err := service.GetOptionByQIDAndSerialNum(q.ID, oSerialNum)
+			if err != nil {
+				c.Error(&gin.Error{Err: errors.New("获取选项信息失败原因: " + err.Error()), Type: gin.ErrorTypeAny})
+				utils.JsonErrorResponse(c, code.ServerError)
+				return
+			}
+			qOptions = append(qOptions, GetOptionCount{
+				SerialNum: op.SerialNum,
+				Content:   op.Content,
+				Count:     count,
+			})
+		}
+		response = append(response, GetSurveyStatisticsResponse{
+			SerialNum:    q.SerialNum,
+			Question:     q.Subject,
+			QuestionType: q.QuestionType,
+			Options:      qOptions,
+		})
+
+		utils.JsonSuccessResponse(c, response)
+	}
 }
