9f6eea617f78d4ac122d7857997cd1945f06385d
Penryn <15158052130@163.com>
2024-12-19 19:17:25 +0800

    fix: 将问卷提交修改成每日0点更新

--- internal/service/redis.go
+++ internal/service/redis.go
@@ -20,7 +20,13 @@ func SetUserLimit(c context.Context, stu_id string, sid int, limit int) error {
 	item := "survey:" + strconv.Itoa(sid) + ":stu_id:" + stu_id
 	// 获取当前时间和第二天零点的时间
 	now := time.Now()
-	tomorrow := now.Add(24 * time.Hour).Truncate(24 * time.Hour)
+	tomorrow := time.Date(
+		now.Year(),
+		now.Month(),
+		now.Day(),
+		0, 0, 0, 0,
+		now.Location(),
+	).Add(24 * time.Hour)
 	duration := time.Until(tomorrow) // 计算当前时间到第二天零点的时间间隔
 	err := redis.RedisClient.Set(c, item, limit, duration).Err()
 	return err
